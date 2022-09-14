d557f7432747bcb9b68fc88fae06a540727d9149
CIT142F-44@M307.UC-BCF.EDU.PH <CIT142F-44@M307.UC-BCF.EDU.PH>
2022-09-14 16:45:17 +0800

    Batch Upload

--- batchupload.bat
+++ batchupload.bat
@@ -1,5 +1,6 @@
 @echo off
-git remote set-url origin https//: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@github.com/carljustinm/CarlMasedman.git
+git remote set-url origin https://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@github.com/carljustinm/CarlMasedman.git
+pause
 git add .
 git commit -m "Batch Upload"
 git push
